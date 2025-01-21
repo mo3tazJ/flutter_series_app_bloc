697788ef83ed9b8d175a494b73fb648bb7f53eee
mo3tazJ <en.mo3taz@gmail.com>
2025-01-22 02:38:42 +0300

    preparing

---
+++ lib/features/characters/business_logic/cubit/characters_state.dart
@@ -0,0 +1,10 @@
+part of 'characters_cubit.dart';
+
+abstract class CharactersState extends Equatable {
+  const CharactersState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CharactersInitial extends CharactersState {}
---
+++ lib/features/episodes/presentation/cubit/episodes_state.dart
@@ -0,0 +1,10 @@
+part of 'episodes_cubit.dart';
+
+abstract class EpisodesState extends Equatable {
+  const EpisodesState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class EpisodesInitial extends EpisodesState {}
